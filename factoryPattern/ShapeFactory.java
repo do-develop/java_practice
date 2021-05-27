5dfc372e33871d4051ac1a2ad5f98d92bd176894
doyoung <doyoh1179@gmail.com>
2021-05-27 22:03:58 +0800

    Design pattern - Factory

---
+++ factoryPattern/ShapeFactory.java
@@ -0,0 +1,20 @@
+package factoryPattern;
+
+public class ShapeFactory {
+	public Shape getShape(String shapeType) {
+		if(shapeType==null) {
+			return null;
+		}
+		if(shapeType.equalsIgnoreCase("Circle")) {
+			return new Circle();
+		}
+		if(shapeType.equalsIgnoreCase("Rectangle")) {
+			return new Rectangle();
+		}
+		if(shapeType.equalsIgnoreCase("Square")) {
+			return new Square();
+		}
+		return null;
+	}
+
+}
