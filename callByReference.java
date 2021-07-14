1548cee19ebfb9b3365fb101a0b327e03ed59479
doyoung <doyoh1179@gmail.com>
2021-07-15 07:34:16 +0800

    Java Example - Call by reference

---
+++ callByReference.java
@@ -0,0 +1,29 @@
+package session3;
+
+import java.awt.Point;
+
+public class callByReference {
+
+	public static void tricky(Point arg1, Point arg2) {
+		//below has effect
+		arg1.x = 100;
+		arg2.y = 50;
+	
+		//below has no effect
+		Point temp = arg1;
+		arg1 = arg2;
+		arg2 = temp;
+	}
+	
+	public static void main(String[] args) {
+		Point pnt1 = new Point(0,0);
+		Point pnt2 = new Point(0,0);
+		System.out.println("X:" + pnt1.x + " Y:" + pnt1.y);
+		System.out.println("X:" + pnt2.x + " Y:" + pnt2.y);
+		System.out.println(" ");
+		tricky(pnt1, pnt2);
+		System.out.println("X:" + pnt1.x + " Y:" + pnt1.y);
+		System.out.println("X:" + pnt2.x + " Y:" + pnt2.y);
+	}
+
+}
